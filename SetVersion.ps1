41a5c56251e3972930b71974f5a67e90978d6ee8
Robin <robin@dapplo.net>
2016-10-31 16:25:22 +0100

    Small fixes

---
+++ SetVersion.ps1
@@ -0,0 +1,10 @@
+# Set the build version in the project.json files
+
+Get-ChildItem . -recurse project.json | 
+		foreach {
+			$projectJson = $_.FullName
+			$jsonContent = Get-Content $projectJson -raw | ConvertFrom-Json
+			echo "Changing Version: $jsonContent.version to $env.APPVEYOR_BUILD_VERSION"
+			$jsonContent.version = $env.APPVEYOR_BUILD_VERSION
+			$jsonContent | ConvertTo-Json  | set-content $_.FullName
+		}
