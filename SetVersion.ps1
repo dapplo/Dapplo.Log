f94ba5c77c7efca87afab26e2d27c3bd904d6b72
Robin <robin@dapplo.net>
2016-10-31 16:29:46 +0100

    Fixing version [release]

--- SetVersion.ps1
+++ SetVersion.ps1
@@ -4,7 +4,8 @@ Get-ChildItem . -recurse project.json |
 		foreach {
 			$projectJson = $_.FullName
 			$jsonContent = Get-Content $projectJson -raw | ConvertFrom-Json
-			echo "Changing Version: $jsonContent.version to $env.APPVEYOR_BUILD_VERSION"
-			$jsonContent.version = $env.APPVEYOR_BUILD_VERSION
+			$version = $jsonContent.version
+			echo "Changing Version: $version to $env:APPVEYOR_BUILD_VERSION"
+			$jsonContent.version = $env:APPVEYOR_BUILD_VERSION
 			$jsonContent | ConvertTo-Json  | set-content $_.FullName
 		}
